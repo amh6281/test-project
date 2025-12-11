000f891a6cf22a8e3ebc335c6f2450bc52f73b8f
amh6281 <amh6281@naver.com>
2025-12-11 18:08:39 +0900

    chore: (root) 폴더 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
-export default function Home() {
+const Home = () => {
   return (
-    <div className='flex min-h-screen items-center justify-center'>
+    <div className='flex h-full items-center justify-center'>
       <main>
         <h1 className='text-3xl font-semibold text-slate-900'>home</h1>
       </main>
     </div>
   );
-}
+};
+
+export default Home;
