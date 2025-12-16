f26c21b87a0c19beade80f2c01ebc22663e8215b
amh6281 <amh6281@naver.com>
2025-12-16 18:22:08 +0900

    chore: 불필요한 파일 제거

--- components/report/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AssetDonutChart } from './AssetDonutChart';
-export { default as DeltaBadge } from './DeltaBadge';
-export { default as DownloadButton } from './DownloadButton';
-export { default as ReportSummaryCard } from './ReportSummaryCard';
